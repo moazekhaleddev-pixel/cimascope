b55a74285b50dd50bdf208dc37054bab85af2a65
MoazKhaled-droid <moazkhaled.dev@gmail.com>
2026-09-16 01:31:33 +0300

    fix: image path

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,10 +1,9 @@
 import { Link } from 'react-router-dom';
 import styles from './Logo.module.css'
-import logoImg from '../../../assets/logo.png'
 export default function Logo() {
   return (
     <Link to="/cimascope/home" className={styles.logo}>
-      <img src={logoImg} alt="logo" width={50} height={35}/>
+      <img src="/assets/logo.png" alt="logo" width={50} height={35}/>
       <span>cima</span>scope
     </Link>
   );
